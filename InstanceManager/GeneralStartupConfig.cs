4bf2ff40e4914fa65349ed2fa59ba9b4a452b5d5
xenu <xenucode@gmail.com>
2023-05-22 16:43:36 +0200

    Stylistic changes for going public

---
+++ InstanceManager/GeneralStartupConfig.cs
@@ -0,0 +1,8 @@
+namespace InstanceManager;
+
+public class StartupConfig
+{
+    public string instance_path, working_directory;
+    public int Xms;
+    public int Xmx;
+}
--- InstanceManager/Program.cs
+++ InstanceManager/Program.cs
@@ -1,29 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using CoolConsole = Colorful.Console;
 using System.Diagnostics;
 using System.Drawing;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using Colorful;
 using InstanceManager;
 using Newtonsoft.Json;
-using SockNet.ServerSocket;
-using SuperSocket.Channel;
 using WebSocketSharp;
 using WebSocketSharp.Server;
-using Console = System.Console;
+using CoolConsole = Colorful.Console;
 
 public class Program
 {
     private static StreamWriter input;
 
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        CoolConsole.WriteAscii("INSTANCE MANAGER",Color.Green);
-        CoolConsole.WriteAscii("0.1.0",Color.Green);
-        We we = new We();
+        CoolConsole.WriteAscii("INSTANCE MANAGER", Color.Green);
+        CoolConsole.WriteAscii("0.1.0", Color.Green);
+        var we = new We();
+        new Thread(Start).Start();
         var result = we.Start();
         for (;;)
         {
@@ -31,27 +25,35 @@ public class Program
         }
     }
 
+    private static void Start(object? obj)
+    {
+    }
+
     public class We
     {
-        public static WebSocketServer wssv = new WebSocketServer();
-        private static string _data ="";
+        public static WebSocketServer wssv = new();
+        private static string _data = "";
         private static Process process;
+
         public async Task<Task> Start()
         {
             SocketConnection();
-            StartupConfig startupConfig = new StartupConfig();
-            using (StreamReader file = File.OpenText("instance_config.json"))
+            var startupConfig = new StartupConfig();
+            using (var file = File.OpenText("instance_config.json"))
             {
-                JsonSerializer serializer = new JsonSerializer();
-                startupConfig= (StartupConfig)serializer.Deserialize(file, typeof(StartupConfig));
+                var serializer = new JsonSerializer();
+                startupConfig = (StartupConfig)serializer.Deserialize(file, typeof(StartupConfig));
             }
-            CoolConsole.WriteLine("Running: "+ startupConfig.instance_path,Color.Chartreuse );
-            CoolConsole.WriteLine("Running with: "+ startupConfig.Xmx+"MB of allocated RAM",Color.Chartreuse );
-            CoolConsole.WriteLine("Running with: "+ startupConfig.Xms+"MB of startup RAM",Color.Chartreuse );
+
+            CoolConsole.WriteLine("Running: " + startupConfig.instance_path, Color.Chartreuse);
+            CoolConsole.WriteLine("Running with: " + startupConfig.Xmx + "MB of allocated RAM", Color.Chartreuse);
+            CoolConsole.WriteLine("Running with: " + startupConfig.Xms + "MB of startup RAM", Color.Chartreuse);
+
             process = new Process();
-            
-            ProcessStartInfo processStartInfo = new ProcessStartInfo("java", "-Xms"+startupConfig.Xms+"M -Xmx"+startupConfig.Xmx+"M -jar "+startupConfig.instance_path+" nogui");
-            processStartInfo.WorkingDirectory = "/home/xenu/TESTS/";
+            var processStartInfo = new ProcessStartInfo("java",
+                "-Xms" + startupConfig.Xms + "M -Xmx" + startupConfig.Xmx + "M -jar " + startupConfig.instance_path +
+                " nogui");
+            processStartInfo.WorkingDirectory = startupConfig.working_directory;
             process.StartInfo = processStartInfo;
             process.StartInfo.RedirectStandardOutput = true;
             process.StartInfo.RedirectStandardError = true;
@@ -64,13 +66,12 @@ public class Program
             process.BeginErrorReadLine();
             Thread.Sleep(2000);
             input = process.StandardInput;
-
             AppDomain.CurrentDomain.ProcessExit += CurrentDomainOnProcessExit;
             //socket server
-
             await process.WaitForExitAsync();
             return Task.CompletedTask;
         }
+
         private static void CurrentDomainOnProcessExit(object? sender, EventArgs e)
         {
             CoolConsole.WriteLine("SHUTTING DOWN !!!");
@@ -79,41 +80,39 @@ public class Program
 
         private static void ProcessOnErrorDataReceived(object sender, DataReceivedEventArgs data)
         {
-            string str = data.Data;
-            CoolConsole.WriteLine("sending:"+str,Color.Red);
+            var str = data.Data;
+            CoolConsole.WriteLine("sending:" + str, Color.Red);
             wssv.WebSocketServices["/ConsoleLogging"].Sessions.Broadcast(str);
         }
 
         private static void ProcessOnOutputDataReceived(object sender, DataReceivedEventArgs data)
         {
-            string str = data.Data;
-            CoolConsole.WriteLine(str,Color.Blue);
+            var str = data.Data;
+            CoolConsole.WriteLine(str, Color.Blue);
             wssv.WebSocketServices["/ConsoleLogging"].Sessions.Broadcast(str);
         }
+
         private static void SocketConnection()
         {
-            wssv = new WebSocketServer ( "ws://127.0.0.1:3141");
-            wssv.AddWebSocketService<ConsoleLogging> ("/ConsoleLogging");
-            wssv.Start ();
+            wssv = new WebSocketServer("ws://0.0.0.0:3141");
+            wssv.AddWebSocketService<ConsoleLogging>("/ConsoleLogging");
+            wssv.Start();
             CoolConsole.WriteLine("Websocket Server Ready", Color.Green);
             CoolConsole.WriteLine("Listening on ws://0.0.0.0:3141/ConsoleLogging", Color.Chartreuse);
         }
+
         public class ConsoleLogging : WebSocketBehavior
         {
             protected override void OnOpen()
             {
                 Send("sending info ...");
-                string s = "sa"; 
-                //CoolConsole.WriteLine("connected client" + _data,Color.Green);
             }
 
-            protected override void OnMessage (MessageEventArgs e)
+            protected override void OnMessage(MessageEventArgs e)
             {
-                CoolConsole.WriteLine("remote> "+ e.Data,Color.Aqua);
+                CoolConsole.WriteLine("remote> " + e.Data, Color.Aqua);
                 input.WriteLine(e.Data);
             }
         }
     }
-    
-    
 }
